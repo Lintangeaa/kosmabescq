3e2932cac2d2d6e5eff31af128836108633cebed
lintangknitto <lintangdandungprakoso.knitto@gmail.com>
2024-09-07 14:30:15 +0700

    update

--- resources/views/master-product/index.blade.php
+++ resources/views/master-product/index.blade.php
@@ -6,17 +6,32 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <h1>Product List</h1>
-            <a href="{{ route('master-product.create') }}">Create New Product</a>
-            <ul>
-                @foreach($products as $product)
-                <li>
-                    <a href="{{ route('master-product.show', $product->id) }}">{{ $product->name }}</a>
-                    <p>Price: {{ $product->price }}</p>
-                </li>
-                @endforeach
-            </ul>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 bg-white p-2 rounded-xl shadow-lg">
+            <div class="flex justify-between items-center">
+                <h1 class="text-lg font-semibold">List Penginapan</h1>
+                <a class="bg-orange-500 p-1 rounded px-2 text-white hover:text-orange-500 hover:bg-white border hover:border-orange-500 transition-all duration-300" href="{{ route('master-product.create') }}">Tambah</a>
+            </div>
+            <table class="w-full overflow-x-auto mt-5">
+                <thead>
+                    <th>
+                        No
+                    </th>
+                    <th>
+                        Nama Penginapan</th>
+                    <th>Harga</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    @foreach($products as $product)
+                    <tr>
+                        <td class="text-center">1</td>
+                        <td class="text-center"><a href="{{ route('master-product.show', $product->id) }}">{{ $product->name }}</a></td>
+                        <td class="text-center">{{ $product->price }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
         </div>
     </div>
 </x-app-layout>
