af422e479a34603d7d71912f6b9e8fb479fac405
Lintangeaa <lindprak@gmail.com>
2025-03-01 12:53:49 +0700

    `UPDATE` : id reservasi buat gunakan random string

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -18,22 +18,11 @@ class Reservation extends Model
 
     public static function generateUniqueReservationId()
     {
-        $today = now()->format('dmY');  // Format tanggal (ddmmyyyy)
-        
-        // Ambil reservasi terakhir untuk hari ini
-        $latestReservation = self::whereDate('created_at', today())->latest()->first();
+        $today = now()->format('dmY');
 
-        // Jika sudah ada reservasi untuk hari ini, ambil nomor urut terakhir
-        if ($latestReservation) {
-            // Ambil 3 karakter terakhir dari ID dan jadikan integer
-            $latestId = (int)substr($latestReservation->id, -2); // Ambil 2 digit terakhir dari ID yang sudah ada
-            $newId = $latestId + 1;  // Tambah satu untuk ID baru
-        } else {
-            $newId = 1;  // Jika tidak ada reservasi, mulai dari 1
-        }
+        $randomNumber = mt_rand(1000000, 9999999); 
 
-        // Format nomor urut dengan 2 digit, misalnya 01, 02, dst.
-        return 'O' . $today . str_pad($newId, 2, '0', STR_PAD_LEFT);  // Hasil ID: RddmmyyyyNN
+        return 'K' . $today . $randomNumber;
     }
 
 
